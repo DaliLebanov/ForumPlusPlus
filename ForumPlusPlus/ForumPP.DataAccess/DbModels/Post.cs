2c4aafcbbbc9795be14a6b1397150cc78175d048
unknown <dali226@yahoo.com>
2020-09-20 17:57:58 +0200

    Initial Created Data Project

---
+++ ForumPlusPlus/ForumPP.DataAccess/DbModels/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumPP.DataAccess.DbModels
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Forum Forum { get; set; }
+        public virtual IEnumerable<PostReply> Replies { get; set; }
+    }
+}
---
+++ ForumPlusPlus/ForumPP.DataAccess/DbModels/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumPP.DataAccess.DbModels
+{
+    public class User:IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
